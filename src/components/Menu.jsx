8ea0cf5262fb41e87573dacd42ad8658af752f36
koderx <haroldc2005@gmail.com>
2023-02-07 22:35:37 -0500

    first commit

---
+++ src/components/Menu.jsx
@@ -0,0 +1,33 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import enyoi from '../assets/enyoi.jpg'
+
+export default function Menu(){
+    const navItems = ['Principal', 'Portafolio', 'Usuarios', 'Administracion', 'Informacion general'];
+    return (
+        <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <img src={enyoi} alt="enyoi" style={{width: "5%", height: "5%", borderRadius: "100%"}} />
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+    )
+}
